9b5fdc781302d4551e38726a1299743010c8c318
NadunOvitigala <nadunovitigala@gmail.com>
2023-07-15 16:44:16 +0530

    terraform code

--- main.tf
+++ main.tf
@@ -89,6 +89,10 @@ resource "aws_route_table" "private" {
         cidr_block = "0.0.0.0/0"
         gateway_id = aws_nat_gateway.nat.id
     }
+
+    tags = {
+        Name = "private route"
+    }
 }
 
 resource "aws_route_table_association" "private" {
